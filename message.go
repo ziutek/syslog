20f2a70bef5eddd844ab4cbdc9341c580239b54a
Michał Derkacz <michal@Lnet.pl>
2012-10-29 20:57:54 +0100

    New methog Message.NetAddr

--- message.go
+++ message.go
@@ -17,6 +17,20 @@ type Message struct {
 	Content   string
 }
 
+// NetAddr only network part of addr as string (IP for UDP or Name for UDS)
+func (m *Message) NetAddr() string {
+	switch a := m.Source.(type) {
+	case *net.UDPAddr:
+		return a.IP.String()
+	case *net.UnixAddr:
+		return a.Name
+	case *net.TCPAddr:
+		return a.IP.String()
+	}
+	// Unknown type
+	return m.Source.String()
+}
+
 func (m *Message) String() string {
 	timeLayout := "2006-01-02 15:04:05"
 	timestampLayout := "01-02 15:04:05"
